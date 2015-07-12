d619d6af90a1eaa60b8cbf1d0f9f0fccd5398db9
Srivatsav Pyda <17srivatsavp@students.harker.org>
2015-07-12 00:05:11 -0700

    1) changed the mgr to handle both types of timestamps 2) added the global file writing

--- Manager.php
+++ Manager.php
@@ -15,8 +15,32 @@
 
 include("script/common/repetitionChecker.php");
 require "script/lib/PHPMailer/PHPMailerAutoload.php";
+
+
+//reading description files
+$has_been_seen = array();
+$seen = fopen("descriptions.txt", "a+");        //change the location as you see fit
+$unknown = fopen("unknown.txt", "a+");
+while (($buffer = fgets($seen)) !== false)
+{
+    $line= explode("@:@", $buffer);
+    $index[0];
+    $value = $line[1];
+    array_push($has_been_seen, $index=>$value);
+}
+
+
+
+
+
+
+
+
+
+
 //$dir = new DirectoryIterator("script/singles/new");		// TODO: RE-ENABLE THIS IN PRODUCTION
-$dir = new DirectoryIterator("script/singles/test");		// KLUDGE FOR TESTING ONLY
+//$dir = new DirectoryIterator("script/singles/test");		// KLUDGE FOR TESTING ONLY
+$dir = new DirectoryIterator("scr");                          // For small scale testing only
 
 /*
  * setup timezone so we don't get errors
@@ -34,7 +58,7 @@ foreach($dir as $file ){
     if($x >2)
         array_push($files, $file->getFilename());
 }
-
+//echo "here\n";
 
 
 /*
@@ -60,11 +84,12 @@ for($indexer = 0; $indexer < sizeof($files); $indexer++)
     $scrape = array();
 
     $fl = $files[$indexer];
+    //echo "Num Scripts: ". sizeof($files)."\n";
     echo "******** agency=$fl\n";
-
+    //if(!$fl = )
     //include("script\\singles\\new\\$fl");
     //include(realpath("script/singles/new/$fl"));
-    include(realpath("script/singles/test/$fl"));
+    include(realpath("scr/$fl"));
 
     $scrape = $incidentList;
 
@@ -72,6 +97,10 @@ for($indexer = 0; $indexer < sizeof($files); $indexer++)
      * insert the agency scrape file calls here
      */
     $size = sizeof(($scrape));
+    if($size == 1)
+    {
+        continue;
+    }
     //echo $scrape[$size - 1]["curlWorking"]."\n";
     //echo
     //var_dump($scrape[$size - 1]);
@@ -86,25 +115,42 @@ for($indexer = 0; $indexer < sizeof($files); $indexer++)
 
     $name = $scrape[$size - 1]["agencyName"];
 
-    $file_path = "./data/$name.txt";
+    $file_path = "data/$name.txt";
     //echo "********** name=$name  file_path=$file_path \n";
 
     $file_fd = fopen($file_path, "a+");
+    echo "file size: " . filesize($file_path). "\n";
+    //die();
     if(filesize($file_path) == 0) {
-        fwrite($file_fd, "$name\tEpoch=0\n");
+        //echo "here";
+        //die();
+        fwrite($file_fd, "$name\tEpoch: 0\n");
     }
-    $Checker->init($file_fd);
+    //die();
+    $Checker->init($file_fd, $scrape);
+    //die();
+    /*if($Checker->has_epoch == false)
+    {
+        for($a = $size - 2; $a >= 0; $a--) 
+        {
+            $arr = $scrape[$a];
+            if($Checker)
+        }
+    }*/
+
     $epoch = $Checker->getEpoch();
     $o_epoch = $epoch;
 
     for($a = $size - 2; $a >= 0; $a--) {
+        //echo "time: ".$time."\n";
         $arr = $scrape[$a];
 	//var_dump($arr);
 
 	/*
          * remember the most recent incident for state file
          */
-        $time = intval($arr["Epoch"]);
+        $time = floatval($arr["Epoch"]);
+        //echo "time: ".$time."\n";
         //echo "                this=".$time."     epoch=".$epoch."  addr=".$incident["Address"]."\n";
         if($time > $epoch) {
             $epoch = $time;
@@ -115,9 +161,13 @@ for($indexer = 0; $indexer < sizeof($files); $indexer++)
         if(!filter($arr, $name))
             continue;
 	*/
-
-        if(!$Checker->checkrep($arr))
-            continue;
+        if($Checker->checkRemaining())
+        {
+            if(!$Checker->checkrep($arr, $a))
+            {
+                continue;
+            }
+        }
 
         $temp1 = $arr["State"];
         $temp2 = $arr["City"];
@@ -141,35 +191,86 @@ for($indexer = 0; $indexer < sizeof($files); $indexer++)
 		"Source: $temp10\n";
 
 
-	echo "          $temp9:  $temp4";
+	//echo "          $temp9:  $temp4";
 //        emailsend($str, $str2);			TODO: RE-ENABLED IN PRODUCTION MODE
 
     }
-
+    //echo "here";
     /*
      * update state file with latest epoch
      */
-    if($epoch > $o_epoch) {
-        $Checker->incidentadd($last_incident);
+    if($Checker->has_epoch == true)
+    {
+        echo "new: ".$epoch."\nold: ". $o_epoch ."\n";
+        if($epoch > $o_epoch) 
+        {
+            $Checker->incidentadd($last_incident);
+        }
+    }
+    else
+    {
+        echo "index: ".$Checker->index."\n";
+        if($Checker->index == -1)
+        {
+            for($a = $size - 2; $a >= 0; $a--) 
+            {
+                $arr = $scrape[$a];
+                
+
+                $temp1 = $arr["State"];
+                $temp2 = $arr["City"];
+                //$county = $arr["County"];
+                $temp3 = $arr["Incident"];
+                $county = $arr["County"];
+                $temp4 = $arr["Address"];
+                $temp5 = $arr["Unit"];
+                $temp6 = $arr["latlng"];
+                $temp7 = $arr["Timestamp"];
+                $temp8 = $arr["Primary Dispatcher #"];
+                $temp9 = $arr["Description"];
+                $temp10 = $arr["Source"];
+
+                $str =  "State: $temp1<br>City/Twp/Box#: $temp2<br>County: $county<br>Incident: $temp3<br>".
+                    "Address: $temp4<br>Unit: $temp5<br>Latlng: $temp6<br>Date: $temp7<br>Primary Dispatcher #: $temp8<br>".
+                "Description: $temp9<br>Source: $temp10<br>";
+
+                $str2 = "State: $temp1\nCity/Twp/Box#: $temp2\nCounty: $county\nIncident: $temp3\nAddress: $temp4\n".
+                "Unit: $temp5\nLatlng: $temp6\nDate: $temp7\nPrimary Dispatcher #: $temp8\nDescription: $temp9\n".
+                "Source: $temp10\n";
+                //emailsend($str, $str2);
+            }
+            echo "no index\n";
+            $Checker->incidentadd($arr);
+        }
+        if($Checker->index != 0 && $Checker->index != -1)
+            $Checker->incidentadd($arr);
+        echo "here\n";
     }
 }
 
-
-function filter($arr, $name)
+/**
+At the beginning of the file, the program reads the contents of the list of all known descriptions
+into an array called $has_been_seen. Each incident in the file is mapped to true or false in the format
+"description:true," so the array has true or false values for each value. In the filter method, if the given description
+is a key within the local array, then it identifies whether the mapped value is true or false and returns it.
+If the key is not found, the description is added to a file called unkown.txt. Periodically, the contents of the unknown file
+must be manually mapped to its corresponding boolean value and put in the seen file.
+*/
+function filter($description)
 {
-    $txt = $arr["Incident"];
-    if($txt =="default")
-        return false;
-    if($txt == "known")
+    if(!array_key_exists($description, $has_been_seen))
+    {
+        fwrite($unknown, "$description\n");
+        array_push($has_been_seen, "unknown");
         return true;
-    if($txt == "unknown") {
-
-        $name_update_path = "./data/incident_updates/$name-updates.txt";
-        $txt = fopen($name_update_path, "a+");
-        $temp = $arr["Description"];
-        $temp += "\n";
-        fwrite($txt, $temp);
-        return true;
+    }
+    else
+    {
+        $value = $has_been_seen[$description];
+        if($value = "false")
+            return false;
+        else
+            return true;
     }
 }
 
--- script/common/repetitionChecker.php
+++ script/common/repetitionChecker.php
@@ -10,14 +10,36 @@ class RepetitionChecker
     private $file;
     private $filename;
     private $epoch;
+	public $has_epoch;
+	private $description;
+	private $address;
+	private $checkrest;
+	private $shouldadd;
+	public $index;
+	
     //private $incident = array();
     //private $timestamp;
 
 
     //public function __construct($fd, $name)
-    public function init($fd)
+    /*public function getnewest()
     {
+    	return $this->newest;
+    }
+    public function setnewest($incident)
+    {
+    	$this->newest = $incident;
+    }*/
+    public function getIndex()
+    {
+    	return $this->index;
+    }
+    public function init($fd, $arr)
+    {
+    	$this->index = -1;
         $this->file = $fd;
+        $this->checkrest = true;
+        $this->shouldadd = false;
         //$this->filename = $name;	// TODO: no need for filename
         //$this->incidents = $incs;
         //$this->timestamp = $num;
@@ -36,54 +58,119 @@ class RepetitionChecker
         {
             $line = $buffer;
         }
-	$line = str_replace("\r", "", $line);	// strip away all possible line breaks
-	$line = str_replace("\n", "", $line);
+		$line = str_replace("\r", "", $line);	// strip away all possible line breaks
+		$line = str_replace("\n", "", $line);
 
-	/*
-    	//echo "      desc=".$desc."*   filename=".$this->filename."*\n";
-        if($line == $this->filename) {
-            //fwrite($this->file, $str);
-            $this->epoch = 0;
-        } else {
-            $end = explode("Epoch: ", $line);
-            $this->epoch = intval($end[1]);
+		/*
+	    	//echo "      desc=".$desc."*   filename=".$this->filename."*\n";
+	        if($line == $this->filename) {
+	            //fwrite($this->file, $str);
+	            $this->epoch = 0;
+	        } else {
+	            $end = explode("Epoch: ", $line);
+	            $this->epoch = intval($end[1]);
+		}
+		*/
+
+		$end = explode("Epoch: ", $line);
+	    if($arr[0]["Epoch"] != "none")
+		{
+		    $this->epoch = floatval($end[1]);
+			echo "       LAST_EPOCH: ".$this->epoch."\n";
+			$this->has_epoch = true;
+		}
+		else
+		{
+			//echo "here2";
+			//echo "type two. \n";
+			//die();
+			$this->has_epoch = false;
+			if($end[1] == "0")
+			{
+				//echo "here";
+				$this->shouldadd = true;
+				$this->description = "";
+				$this->address = "";
+			}
+			else
+			{
+				//echo $line."\n";
+				$end = explode("\t", $line);
+				$desc = substr($end[0], 13, strlen($end[0]));
+				$addr = substr($end[1], 9, strlen($end[1]));
+				$this->description = $desc;
+				$this->address = $addr;
+				echo "description: ".$desc."\naddress: ".$addr."\n";
+			}
+
+			//die();
+	    }
 	}
-	*/
-
-	$end = explode("Epoch: ", $line);
-    if(sizeof($end) > 1)
-	    $this->epoch = intval($end[1]);
-	echo "       LAST_EPOCH: ".$this->epoch."\n";
-
-    }
 
 
     public function getEpoch() {
     	return $this->epoch;
     }
-
-    public function incidentadd($in) {
-	//var_dump($in);
-	$str = "description: ".$in["Description"]."\taddress: ".$in["Address"].
+	public function getAddr()
+	{
+		return $this->address;
+	}
+	public function getDesc()
+	{
+		return $this->description;
+	}
+	public function checkRemaining()
+	{
+		return $this->checkrest;
+	}
+    public function incidentadd($in) 
+	{
+		//var_dump($in);
+		$str = "description: ".$in["Description"]."\taddress: ".$in["Address"].
 		"\tunits: ".$in["Unit"]."\tEpoch: ".$in["Epoch"]."\n";
         fwrite($this->file, $str);
 
-	echo "***** NEW_EPOCH: ".$str."\n";
+		//echo "***** NEW_EPOCH: ".$str."\n";
     }
-
-    public function checkrep($incident)
+	
+    public function checkrep($incident, $ind)
     {
+    	if($this->shouldadd == true)
+    	{
+    		//$this->shouldadd = false;
+    		//$this->newest = $incident;
+    		echo "shouldadd\n";
+    		return true;            
+    	}
         $time = $incident["Epoch"];
         $desc = $incident["Description"];
         $addr = $incident["Address"];
         //$str = "Description: $desc\tEpoch: $time\n";
-
-	//echo "prev_time=".$this->epoch."    current_time=". $time."   addr=".$addr;
-	if ($time > $this->epoch) {
-	    //echo "....TRUE\n";
-	    return true;
-	}
-	//echo "....FALSE\n";
-	return false;
+		//echo "prev_time=".$this->epoch."    current_time=". $time."   addr=".$addr;
+		if($this->has_epoch)
+		{
+			echo "has epoch \n";
+			//echo "new: ".$time."\nold: $this->epoch\n";
+			//echo "here3";
+			if ($time > $this->epoch) {
+			//echo "....TRUE\n";
+				return true;
+			}
+			//echo "....FALSE\n";
+			return false;
+		}
+		else
+		{
+			echo "no epoch\n";
+			if($desc == $this->description && $addr == $this->address)
+			{
+				$this->index = $ind;
+				echo "index: ".$this->index."\n";
+				$this->checkrest = false;
+				return false;
+			}
+			//$this->newest = $incident;
+			return false;
+		}
     }
 }
